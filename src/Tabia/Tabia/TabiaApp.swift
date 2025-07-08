ec27dce7fab1ac21fa7a1f9b44f9255a28dbb1ea
JM Bell <justinmbell1997@gmail.com>
2025-07-07 20:47:49 -0500

    created source code

---
+++ src/Tabia/Tabia/TabiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabiaApp.swift
+//  Tabia
+//
+//  Created by Justin Bell on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TabiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
